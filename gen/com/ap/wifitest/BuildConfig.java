bdaee60153c7b8ab77565b05fd917529b5d3b57d
prat <ptanapaisankit@Prat-PC.AetherPal.local>
2013-06-17 15:29:42 -0400

    initial commit

---
+++ gen/com/ap/wifitest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ap.wifitest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ap/wifitest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.ap.wifitest;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	private static IntentFilter mIntentFilter;
+	private static WifiDataReceiver receiver;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mIntentFilter = new IntentFilter();
+		mIntentFilter
+				.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+		receiver = new WifiDataReceiver();
+		registerReceiver(receiver,
+				mIntentFilter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	class WifiDataReceiver extends BroadcastReceiver {
+	    public void onReceive(Context c, Intent intent) {
+	        Log.v("msg", "onReceive WifiDataReceiver");
+	        ConnectivityManager cm = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+	        NetworkInfo.State mWifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
+	        Log.v("msg", "mWifi : " + mWifi);
+	        if (mWifi != NetworkInfo.State.CONNECTED){
+	            Log.v("msg", "Disconnect  WifiDataReceiver");
+	        } else {
+	            Log.v("msg", "connected");
+	        }
+	    }
+	}
+}
